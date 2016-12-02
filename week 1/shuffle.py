1b98774a4a9a96e11d7dc3b43159d2b237465e5a
Joanna Malanowska <malanowj@uni.coventry.ac.uk>
2016-12-02 16:42:15 +0000

    Update shuffle.py

--- week 1/shuffle.py
+++ week 1/shuffle.py
@@ -1,12 +1,13 @@
 import random
 
 def randomList(a):
-    a=[1,2,3,4,5,6,7,8,9]
+    
     b=[]
     for i in range(len(a)):
         element=random.choice(a)
         a.remove(element)
         b.append(element)
-        return(b)
-
+        if len(a)==0:
+            print(b)
+a=[1,2,3,4,5,6,7,8,9]
 randomList(a)
